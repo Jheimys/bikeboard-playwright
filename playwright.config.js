5e8273fc6a3c945837abe16d61d16be1ba27c8d9
Jheimys Bezerra Bassani <jheimysbb@gmail.com>
2026-07-14 12:29:02 +0100

    teste de navegadores executado por cli

--- playwright.config.js
+++ playwright.config.js
@@ -41,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+       name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+       name: 'webkit',
+       use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
@@ -79,3 +79,4 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
   },
 });
+
